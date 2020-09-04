607ba4eadc09dffc781ee27f870f3c6591f77866
Hristo <i4kowee@gmail.com>
2020-09-04 12:19:35 +0300

    Create Database
    Simple project with React and Asp.Net MVC, Create Database with Entity Framework and MSSQL

---
+++ React_And_Asp.Net/ReactAspDemoProject/ReactAspDemoProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ReactAspDemoProject.MvcApplication" Language="C#" %>
---
+++ React_And_Asp.Net/ReactAspDemoProject/ReactAspDemoProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ReactAspDemoProject.Startup))]
+namespace ReactAspDemoProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
